779456e9931d237adda5f1f5e86fb869aa25c873
@adriafrixas <adrifre7@hotmail.com>
2017-03-21 17:10:32 +0100

    Proyecto cuentas

--- GetStarted/GetStarted/MainPage.xaml.cs
+++ GetStarted/GetStarted/MainPage.xaml.cs
@@ -9,9 +9,18 @@ namespace GetStarted
 {
     public partial class MainPage : ContentPage
     {
+        public int NClicks { get; set; }
+        private string _frase = "Veces has clicado el botón";
         public MainPage()
         {
             InitializeComponent();
+            counterTxt.Text = "Aqui va a ir mi texto";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            NClicks++;
+            counterTxt.Text = NClicks + " " + _frase;
         }
     }
 }
