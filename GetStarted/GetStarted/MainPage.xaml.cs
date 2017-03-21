c11ef73d26db02a3267f47a13b9f5016fc4bc18b
@adriafrixas <adrifre7@hotmail.com>
2017-03-21 17:31:47 +0100

    Binding observable collection

--- GetStarted/GetStarted/MainPage.xaml.cs
+++ GetStarted/GetStarted/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,26 @@ namespace GetStarted
     public partial class MainPage : ContentPage
     {
         public int NClicks { get; set; }
+        private string _registro = "Registro";
         private string _frase = "Veces has clicado el botón";
+        public string TextoAplicacion { get; set; }
+        public ObservableCollection<string> Registros { get; set; }
         public MainPage()
         {
             InitializeComponent();
             counterTxt.Text = "Aqui va a ir mi texto";
+            TextoAplicacion = "Pincha el botón tantas veces como quieras";
+            Registros = new ObservableCollection<string>();
+            Registros.Add(_registro + " " + NClicks);
+            BindingContext = this;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             NClicks++;
             counterTxt.Text = NClicks + " " + _frase;
+            Registros.Add(_registro + " " + NClicks);
+            TextoAplicacion = "Estate quieto";
         }
     }
 }
