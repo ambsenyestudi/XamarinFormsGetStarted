a4f025221b2007fd7efe7ee823d421bbaab65e92
@adriafrixas <adrifre7@hotmail.com>
2017-03-22 19:26:23 +0100

    comment

--- GetStarted/GetStarted/MainPage.xaml.cs
+++ GetStarted/GetStarted/MainPage.xaml.cs
@@ -30,6 +30,7 @@ namespace GetStarted
             NClicks++;
             counterTxt.Text = NClicks + " " + _frase;
             Registros.Add(_registro + " " + NClicks);
+            //Esto no funciona
             TextoAplicacion = "Estate quieto";
         }
     }
