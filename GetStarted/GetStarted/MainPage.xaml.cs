b407da5f6e42c92d18cb5618a93dfe5e6cb98e6c
@adriafrixas <adrifre7@hotmail.com>
2017-03-22 19:56:47 +0100

    ViewModel

--- GetStarted/GetStarted/MainPage.xaml.cs
+++ GetStarted/GetStarted/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GetStarted.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -10,28 +11,11 @@ namespace GetStarted
 {
     public partial class MainPage : ContentPage
     {
-        public int NClicks { get; set; }
-        private string _registro = "Registro";
-        private string _frase = "Veces has clicado el botón";
-        public string TextoAplicacion { get; set; }
-        public ObservableCollection<string> Registros { get; set; }
+        
         public MainPage()
         {
             InitializeComponent();
-            counterTxt.Text = "Aqui va a ir mi texto";
-            TextoAplicacion = "Pincha el botón tantas veces como quieras";
-            Registros = new ObservableCollection<string>();
-            Registros.Add(_registro + " " + NClicks);
-            BindingContext = this;
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            NClicks++;
-            counterTxt.Text = NClicks + " " + _frase;
-            Registros.Add(_registro + " " + NClicks);
-            //Esto no funciona
-            TextoAplicacion = "Estate quieto";
+            BindingContext = new MainViewModel();
         }
     }
 }
---
+++ GetStarted/GetStarted/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GetStarted.ViewModel
+{
+    public class MainViewModel:BindableObject
+    {
+
+        private string _registro = "Registro";
+        private string _frase = "Veces has clicado el botón";
+        public int NClicks { get; set; }
+        private string _textoAplicacion;
+
+        public string TextoAplicacion
+        {
+            get { return _textoAplicacion; }
+            set
+            {
+                _textoAplicacion = value;
+                OnPropertyChanged("TextoAplicacion");
+            }
+        }
+        private string _textInfo;
+
+        public string TextInfo
+        {
+            get { return _textInfo; }
+            set
+            {
+                _textInfo = value;
+                OnPropertyChanged("TextInfo");
+            }
+        }
+
+        private ICommand _clickCommand;
+
+        public ICommand ClickCommand
+        {
+            get { return _clickCommand; }
+            set {
+                    _clickCommand = value;
+                OnPropertyChanged("ClickCommand");
+            }
+        }
+
+
+
+        public ObservableCollection<string> Registros { get; set; }
+        public MainViewModel()
+        {
+            Registros = new ObservableCollection<string>();
+            Registros.Add(_registro + " " + NClicks);
+            TextoAplicacion = "Un simple texto";
+            TextInfo="Aqui va a ir mi texto";
+            ClickCommand = new Command(Click);
+        }
+
+        public void Click()
+        {
+            NClicks++;
+            TextInfo = NClicks + " " + _frase;
+            Registros.Add(_registro + " " + NClicks);
+            //Esto no funciona
+            TextoAplicacion = "Estate quieto";
+        }
+    }
+}
